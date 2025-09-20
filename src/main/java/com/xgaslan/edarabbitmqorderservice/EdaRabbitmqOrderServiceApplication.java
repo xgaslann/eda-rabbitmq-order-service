88b6012152a52551f5b086fb9afecb295db08a37
xgaslann <gorkem_aslan0@hotmail.com>
2025-09-20 22:44:53 +0300

    xgaslan initial

---
+++ src/main/java/com/xgaslan/edarabbitmqorderservice/EdaRabbitmqOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xgaslan.edarabbitmqorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaRabbitmqOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaRabbitmqOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xgaslan/edarabbitmqorderservice/EdaRabbitmqOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgaslan.edarabbitmqorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaRabbitmqOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
